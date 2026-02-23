5b4a9e5814c2c1b554cbcf9914043859fdd90c6b
shashish-b <shashishbommanaboina3@gmail.com>
2026-02-23 11:21:42 +0530

    Add files via upload

---
+++ 02_assovciativity.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3;
+    int b = 6;
+    int c = 9;
+    printf("The value is %d\n", a*b/c + 7);
+      printf("The value is %d\n", 2*b/c*3 + 7*a);
+    return 0;
+}
+
---
+++ 02_typecasting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>v
+
+int main(){
+    int n = 35;
+    float m = 42.73;
+    n = (int)m;
+    printf("%d\n", n);
+    return 0;
+
+}
